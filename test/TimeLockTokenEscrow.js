7d8b0b7c5e7cf7e631227dd54da6389cc205c748
Vincent de Almeida <vince0656@googlemail.com>
2020-01-20 11:44:19 +0000

    Revert unit tests for locking up tokens

--- test/TimeLockTokenEscrow.js
+++ test/TimeLockTokenEscrow.js
@@ -15,10 +15,10 @@ contract('TimeLockTokenEscrow tests', function ([creator, beneficiary1, ...accou
    });
 
    describe('lock() - Locking up tokens', function() {
-      describe.only('happy path', function() {
-         it('Locks up a specified amount of tokens', async function() {
-            const amountToLockUp = new BN('5000');
+      const amountToLockUp = new BN('5000');
 
+      describe('happy path', function() {
+         it('Locks up a specified amount of tokens', async function() {
             // Approve the contract first and check its allowance after
             await this.token.approve(this.timeLockTokenEscrow.address, amountToLockUp, {from: creator});
 
@@ -36,7 +36,42 @@ contract('TimeLockTokenEscrow tests', function ([creator, beneficiary1, ...accou
       });
 
       describe('requires', function() {
+         it('Fails to lockup tokens for address zero', async function() {
+            await expectRevert(
+                this.timeLockTokenEscrow.lock(ZERO_ADDRESS, amountToLockUp, '0', {from: creator}),
+                "You cannot lock up tokens for the zero address"
+            );
+         });
 
+         it('Fails when amount is zero', async function() {
+            await expectRevert(
+                this.timeLockTokenEscrow.lock(beneficiary1, '0', '0', {from: creator}),
+                "Lock up amount of zero tokens is invalid"
+            );
+         });
+
+         it('Fails when tokens have already been locked up for a beneficiary', async function() {
+            await this.token.approve(this.timeLockTokenEscrow.address, amountToLockUp, {from: creator});
+
+            (await this.timeLockTokenEscrow.approvalAmount(creator)).should.be.bignumber.equal(amountToLockUp);
+
+            // Lock up the tokens
+            const oneHourFromNow = new BN((now() + (60*60)).toString());
+            await this.timeLockTokenEscrow.lock(beneficiary1, amountToLockUp, oneHourFromNow, {from: creator});
+
+            // Try to lock up again
+            await expectRevert(
+              this.timeLockTokenEscrow.lock(beneficiary1, amountToLockUp, '0', {from: creator}),
+                "Tokens have already been locked up for the given address"
+            );
+         });
+
+         it('Fails when the contract is not approved to escrow tokens', async function() {
+            await expectRevert(
+                this.timeLockTokenEscrow.lock(beneficiary1, amountToLockUp, '0', {from: creator}),
+                "The contract does not have enough of an allowance to escrow"
+            );
+         });
       });
    });
 });
