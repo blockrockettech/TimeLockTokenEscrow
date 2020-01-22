d57616e306d9123c1eb4e7f47f72d294f5285919
Vincent de Almeida <vince0656@googlemail.com>
2020-01-22 11:40:05 +0000

    Basic locking dapp

---
+++ front-end/src/utils.js
@@ -0,0 +1,13 @@
+function getContractAddressFromTruffleConf(truffleConf, chainId) {
+    if(!truffleConf || !chainId) return '';
+    const {networks} = truffleConf;
+    if (networks[chainId.toString()]) {
+        const address = networks[chainId.toString()].address;
+        return address ? address : '';
+    }
+    return '';
+}
+
+export default {
+    getContractAddressFromTruffleConf
+}
---
+++ front-end/src/views/Home.vue
@@ -0,0 +1,120 @@
+<template>
+    <div class="container mt-4">
+        <h1>Time Lock Token Escrow (XTP)</h1>
+        <div>
+            <label class="fixed-width-label text-right" for="inputBeneficiary">Beneficiary:</label>
+            <input type="text"
+                   id="inputBeneficiary"
+                   class="ml-2 form-control fixed-width-input d-inline-block"
+                   placeholder="0x123..."
+                   v-model="form.beneficiary"/>
+        </div>
+        <div class="mt-1">
+            <label class="fixed-width-label text-right" for="inputAmount">Amount: </label>
+            <input type="text"
+                   id="inputAmount"
+                   class="ml-2 form-control fixed-width-input d-inline-block"
+                   placeholder="500"
+                   v-model="form.amount"/>
+        </div>
+        <div class="mt-1">
+            <label class="fixed-width-label text-right" for="inputLockedUntil">Locked Until: </label>
+            <input type="text"
+                   id="inputLockedUntil"
+                   class="ml-2 form-control fixed-width-input d-inline-block"
+                   placeholder="10/05/2020"
+                   v-model="form.lockedUntil"/>
+        </div>
+        <b-button variant="primary" class="mt-2" @click="lockupTokens">
+            <span v-if="!lockingUp">Lockup</span>
+            <SmallSpinner v-else />
+        </b-button>
+    </div>
+</template>
+
+<script>
+    import {ethers} from 'ethers';
+    import utils from '../utils';
+    import TimeLockTokenEscrow from '../truffleconf/TimeLockTokenEscrow';
+    import GenericERC20Token from '../truffleconf/GenericERC20Token';
+
+    import SmallSpinner from "../components/SmallSpinner";
+
+    export default {
+        name: 'home',
+        components: {SmallSpinner},
+        async created() {
+            await window.ethereum.enable();
+            this.web3.provider = new ethers.providers.Web3Provider(web3.currentProvider);
+            this.web3.signer = this.web3.provider.getSigner();
+            this.web3.chain = await this.web3.provider.getNetwork();
+
+            const escrowContractAddress = utils.getContractAddressFromTruffleConf(TimeLockTokenEscrow, this.web3.chain.chainId);
+            this.web3.escrowContract = new ethers.Contract(
+                escrowContractAddress,
+                TimeLockTokenEscrow.abi,
+                this.web3.signer,
+            );
+
+            const tokenAddress = utils.getContractAddressFromTruffleConf(GenericERC20Token, this.web3.chain.chainId);
+            this.web3.genericERC20TokenContract = new ethers.Contract(
+                tokenAddress,
+                GenericERC20Token.abi,
+                this.web3.signer
+            );
+        },
+        data() {
+            return {
+                form: {
+                    beneficiary: '',
+                    amount: '',
+                    lockedUntil: '',
+                },
+                web3: {
+                    provider: null,
+                    signer: null,
+                    chain: null,
+                    escrowContract: null,
+                    genericERC20TokenContract: null
+                },
+                lockingUp: false
+            }
+        },
+        methods: {
+            async lockupTokens() {
+                this.lockingUp = true;
+
+                const escrowContractAddress = utils.getContractAddressFromTruffleConf(TimeLockTokenEscrow, this.web3.chain.chainId);
+
+                // Approve
+                const approveTx = await this.web3.genericERC20TokenContract.approve(escrowContractAddress, this.form.amount);
+
+                // Lockup
+                const lockupTx = await this.web3.escrowContract.lock(
+                    this.form.beneficiary,
+                    this.form.amount,
+                    this.form.lockedUntil,
+                    {
+                        gasLimit: 250000
+                    }
+                );
+
+                // Wait for 1 confirmation for both transactions
+                await approveTx.wait(1);
+                await lockupTx.wait(1);
+
+                this.lockingUp = false;
+            }
+        }
+    }
+</script>
+
+<style scoped>
+    .fixed-width-label {
+        width: 100px;
+    }
+
+    .fixed-width-input {
+        width: 250px;
+    }
+</style>
