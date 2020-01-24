49bba777b32c40e94e8ff1161e635541954f52af
James Morgan <james.morgan.e@googlemail.com>
2020-01-24 08:42:56 +0000

    Adding revert lock and tests

--- smart-contracts/contracts/TimeLockTokenEscrow.sol
+++ smart-contracts/contracts/TimeLockTokenEscrow.sol
@@ -6,9 +6,16 @@ import "@openzeppelin/contracts/utils/ReentrancyGuard.sol";
 contract TimeLockTokenEscrow is ReentrancyGuard {
 
     event Lockup(
+        address indexed _creator,
         address indexed _beneficiary,
         uint256 indexed _amount,
         uint256 _lockedUntil
+    );
+
+    event LockupReverted(
+        address indexed _creator,
+        address indexed _beneficiary,
+        uint256 indexed _amount
     );
 
     event Withdrawal(
@@ -17,6 +24,7 @@ contract TimeLockTokenEscrow is ReentrancyGuard {
     );
 
     struct TimeLock {
+        address creator;
         uint256 amount;
         uint256 lockedUntil;
     }
@@ -36,14 +44,29 @@ contract TimeLockTokenEscrow is ReentrancyGuard {
         require(token.allowance(msg.sender, address(this)) >= _amount, "The contract does not have enough of an allowance to escrow");
 
         beneficiaryToTimeLock[_beneficiary] = TimeLock({
-            amount: _amount,
-            lockedUntil: _lockedUntil
+            creator : msg.sender,
+            amount : _amount,
+            lockedUntil : _lockedUntil
         });
 
         bool transferSuccess = token.transferFrom(msg.sender, address(this), _amount);
         require(transferSuccess, "Failed to escrow tokens into the contract");
 
-        emit Lockup(_beneficiary, _amount, _lockedUntil);
+        emit Lockup(msg.sender, _beneficiary, _amount, _lockedUntil);
+    }
+
+    function revertLock(address _beneficiary) external nonReentrant {
+        TimeLock storage lockup = beneficiaryToTimeLock[_beneficiary];
+        require(lockup.creator == msg.sender, "Cannot revert a lock unless you are the creator");
+        require(lockup.amount > 0, "There are no tokens left to revert lock up for this address");
+
+        uint256 transferAmount = lockup.amount;
+        lockup.amount = 0;
+
+        bool transferSuccess = token.transfer(lockup.creator, transferAmount);
+        require(transferSuccess, "Failed to send tokens back to lock creator");
+
+        emit LockupReverted(msg.sender, _beneficiary, transferAmount);
     }
 
     function withdrawal(address _beneficiary) external nonReentrant {
